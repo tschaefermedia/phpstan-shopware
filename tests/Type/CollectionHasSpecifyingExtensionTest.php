900b1e7f594bf1e74b3b22b2b603294cc799aca4
Soner Sayakci <s.sayakci@gmail.com>
2025-01-28 13:31:32 +0100

    feat: add CollectionHasSpecifyingExtension

---
+++ tests/Type/CollectionHasSpecifyingExtensionTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Shopware\PhpStan\Tests\Type;
+
+use PHPStan\Testing\TypeInferenceTestCase;
+
+class CollectionHasSpecifyingExtensionTest extends TypeInferenceTestCase
+{
+    public function testCollectionHas(): void
+    {
+        foreach (static::gatherAssertTypes(__DIR__ . '/../data/CollectionHasSpecifyingExtension/collection_has.php') as $args) {
+            // because of the autoload issue we can not use data providers as phpstan does itself,
+            // therefore we need to rely on this hacks
+            $assertType = array_shift($args);
+            $file = array_shift($args);
+
+            $this->assertFileAsserts($assertType, $file, ...$args);
+        }
+    }
+
+    public static function getAdditionalConfigFiles(): array
+    {
+        return [
+            __DIR__ . '/../data/CollectionHasSpecifyingExtension/extension.neon',
+        ];
+    }
+}
